6162dc837a4cee7d26b454144706c6de91b832b1
Pepper Urbaniak <pepper.urbaniak@gmail.com>
2017-03-03 06:57:59 -0800

    moved middleware to own file, added style

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
 var express = require('express');
 var app = express();
 var PORT = '3000';
+var middleware = require('./middleware.js');
 
-var middleware = {
-  requireAuthentication: function (req, res, next) {
-    console.log('private route hit:');
-    next();
-  },
-  logger: function (req, res, next) {
-    var date = new Date().toString();
-    console.log('Request: ' + date + ' ' + req.method + ' ' + req.originalUrl );
-    next();
-  }
-};
 
 app.use(middleware.logger);
 
@@ -23,7 +13,7 @@ app.use(middleware.logger);
 // });
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
-  res.send('About Us!');
+  res.send('<h1>About Us!</h1>');
 });
 
 app.use(express.static(__dirname + '/public'));
